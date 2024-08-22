072772d84c3aca13158e80d1ec1d3117d52dedf7
Melissa <meliza.osorio@upb.edu.co>
2024-08-22 12:55:08 -0500

    version 2.1

--- app/src/main/java/com/upb/myrestaurant/MainActivity.java
+++ app/src/main/java/com/upb/myrestaurant/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 public class MainActivity extends AppCompatActivity {
@@ -30,18 +31,28 @@ public class MainActivity extends AppCompatActivity {
     }
     public void abrirReserva (View v){
 
-        String numero = "+57 numero de alguien";
-        String url = "https://api.whatsapp.com/send?phone=" + numero;
+        String numero = "+573167497545";
+        String mensaje = "Hola, quiero hacer una reserva";
+        String url = "https://api.whatsapp.com/send?phone=" + numero + "&text=" + Uri.encode(mensaje);
         String whatsApp = "com.whatsapp";
         PackageManager pm = getPackageManager();
-        Intent i = pm.getLaunchIntentForPackage(whatsApp);
+
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
         startActivity(i);
+
 
     }
     public void abrirPedido (View v){
 
-        Intent i;
-        i = new Intent(this, PedidoActivity.class);
+        String numero = "+573042081589";
+        String mensaje = "Hola, quiero hacer un pedido";
+        String url = "https://api.whatsapp.com/send?phone=" + numero + "&text=" + Uri.encode(mensaje);
+        String whatsApp = "com.whatsapp";
+        PackageManager pm = getPackageManager();
+
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
         startActivity(i);
 
     }
